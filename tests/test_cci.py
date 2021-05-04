0505086ebb4205d7f4ac1f5f080a4394b56cb241
David Lougheed <david.lougheed@gmail.com>
2021-05-04 10:37:52 -0400

    test: fix cci test

--- tests/test_cci.py
+++ tests/test_cci.py
@@ -9,7 +9,7 @@ def test_cci():
     o1 = os.path.join(os.path.dirname(__file__), "vcfs", "cci.vcf.gz")
     o2 = os.path.join(os.path.dirname(__file__), "vcfs", "cci.vcf.gz.tbi")
 
-    copy_compress_index(f)
+    copy_compress_index([f])
 
     assert os.path.exists(o1)
     assert os.path.exists(o2)
